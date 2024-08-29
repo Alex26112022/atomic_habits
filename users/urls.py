db15af7eba97db8f01ebd5b1ccd63701b4373b58
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-08-29 15:54:25 +0300

    add users

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView, \
+    TokenRefreshView
 
 from users.apps import UsersConfig
+from users.views import UserListAPIView, UserRetrieveAPIView, \
+    UserCreateAPIView, UserUpdateAPIView, UserDestroyAPIView
 
 app_name = UsersConfig.name
 
